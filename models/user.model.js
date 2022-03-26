12b0ef0157c7d47131270547daf5343fdd2c2870
elmehdi rhindi <g6handi@gmail.com>
2022-03-26 15:40:09 +0100

    add first and lst name to user model

--- models/user.model.js
+++ models/user.model.js
@@ -3,6 +3,14 @@ const { Schema, model } = mongoose;
 
 // create a schema
 const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     unique: true,
