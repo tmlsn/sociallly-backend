5d984cb3e73e8625aeb50424844fe4a0befd0362
elmehdi rhindi <g6handi@gmail.com>
2022-03-31 18:59:41 +0200

    change some comments

--- routes/tweet.routes.js
+++ routes/tweet.routes.js
@@ -24,7 +24,6 @@ router.get("/:id", async (req, res) => {
 });
 
 // delete tweet by id
-// TODO: you cannot delete a tweet if you are not the owner
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   const tweet = await Tweet.findById(id);
@@ -37,20 +36,17 @@ router.delete("/:id", async (req, res) => {
 });
 
 // edit tweet by id
-// TODO: you cannot edit a tweet if you are not the owner
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { content } = req.body;
-  let tweet = await Tweet.findById(id)
+  let tweet = await Tweet.findById(id);
   if (tweet.user.toString() === req.jwtPayload.user._id) {
-    tweet.content = content
-    tweet = await tweet.save()
+    tweet.content = content;
+    tweet = await tweet.save();
     res.status(200).json(tweet);
   } else {
     res.status(400).json("unauthorized");
   }
-  //const tweet = await Tweet.findByIdAndUpdate(id, { content }, { new: true });
-  //res.status(200).json(tweet);
 });
 
 module.exports = router;
